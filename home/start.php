45ed70e16802b317f1123c5f92c89cba58106696
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2020-04-09 15:01:33 +0530

    Update start.php

---
+++ home/start.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head> <title>RISK ANALYZER FOR COVID 19</title> </head>
+<body>
+<h1>Welcome to Risk Analyzer for COVID 19</h1>
+<h2>To Start Analyzing Please Enter Some Information</h2></br>
+
+<p>welcome <?php
+    echo $_POST["name"];
+            ?>
+            </p>
+
+</body>
+</html>
