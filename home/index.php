e38c1d58ae40ac4493d189fc5852e732c171dc93
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2020-04-09 21:01:28 +0530

    Update index.php

--- home/index.php
+++ home/index.php
@@ -1,12 +1,17 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head> <title>RISK ANALYZER FOR COVID 19</title> </head>
 <body>
 <h1>Welcome to Risk Analyzer for COVID 19</h1>
 <h2>To Start Analyzing Please Enter Some Information</h2></br>
+
 <form action="start.php" method="POST">
-    Name: <input type="text" name="name" required><br>
-    E-mail: <input type="text" name="email" required><br>
+    Name * : <input type="text" name="name" required><br>
+    Age * : <input type="text" name="age" required><br>
+    Phone(optional): <input type="text" name="phone"><br>
     <input type="submit">
     </form>
 
