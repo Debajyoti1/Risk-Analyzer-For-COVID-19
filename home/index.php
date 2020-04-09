e019b806970b26c0bc2df9cbe3d9774ae32be45f
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2020-04-09 14:53:38 +0530

    Create index.php

---
+++ home/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head> <title>RISK ANALYZER FOR COVID 19</title> </head>
+<body>
+<h1>Welcome to Risk Analyzer for COVID 19</h1>
+<h2>To Start Analyzing Please Enter Some Information</h2></br>
+<form action="start.php" method="POST">
+    Name: <input type="text" name="name" required><br>
+    E-mail: <input type="text" name="email" required><br>
+    <input type="submit">
+    </form>
+
+</body>
+</html>
