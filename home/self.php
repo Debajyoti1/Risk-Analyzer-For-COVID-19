cb27549ab16e1517cd5133274ff12818d13c85d6
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2020-04-13 20:35:59 +0530

    Update self.php

--- home/self.php
+++ home/self.php
@@ -10,55 +10,157 @@
     $_SESSION["q6"]=$_POST["q6"];
 ?>
 <!DOCTYPE html>
-<html>
-<head> <title>RISK ANALYZER FOR COVID 19</title> </head>
-<body>
-<h1>Welcome to Risk Analyzer for COVID 19</h1>
-<h2>Personal Details</h2></br>
-<form action="/result.php" method="POST">
-1.	Tell us if you ever went outside during these lockdown days?
-  <input type="radio" id="yes" name="q7" value="Yes" checked>
-  <label for="yes">Yes</label><br>
-  <input type="radio" id="no" name="q7" value="No">
-  <label for="no">No</label><br>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Risk Analyzer</title>
+    <link rel="stylesheet" href="css/style_self.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+  </head>
+  <body>
+    <nav>
+        <input type="checkbox" id="check">
+        <label for="check">
+          <i class="fas fa-bars" id="btn"></i>
+          <i class="fas fa-times" id="cancel"></i>
+        </label>
+        <img src="gg.png" alt="covid19 risk checker">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">News</a></li>
+          <li><a href="#">Map</a></li>
+          <li><a href="#">FAQ</a></li>
+          <li><a href="#">how to use</a></li>
+        </ul>
+      </nav>
+	  
+	  <div class="page-content">
+		
+		 <form action="/result.php" method="POST">				
+			 	<div class="checkbox">
+			 	 <p>1.	Tell us if you ever went outside during these lockdown days?</p>
+		    	<label>
+			     <input type="radio" id="yes" name="q1" value="Yes" checked>
+				 <span class="yes">Yes</span>
+		    	</label>
+		    	<label>
+ 				 <input type="radio" id="no" name="q1" value="No">
+ 				 <span class="no">No</span>
+		    	</label>
+		    	</div>
+			   
+			   <div class="checkbox1">
+			 	 <p>2.	Tell us honestly if you have fever?</p>
+		    	<label>
+			     <input type="radio" id="yes" name="q2" value="Yes" checked>
+				 <span class="yes">Yes</span>
+		    	</label>
+		    	<label>
+ 				 <input type="radio" id="no" name="q2" value="No">
+ 				 <span class="no">No</span>
+		    	</label>
+		    	</div>
+			 
+			  <div class="checkbox2">
+			 	 <p>3.	Tell us honestly if you have headache?</p>
+		    	<label>
+			     <input type="radio" id="yes" name="q3" value="Yes" checked>
+				 <span class="yes">Yes</span>
+		    	</label>
+		    	<label>
+ 				 <input type="radio" id="no" name="q3" value="No">
+ 				 <span class="no">No</span>
+		    	</label>
+		    	</div>
+			 
+			 <div class="checkbox3">
+			 	 <p>4.	Tell us honestly if you have cough and cold?</p>
+		    	<label>
+			     <input type="radio" id="yes" name="q4" value="Yes" checked>
+				 <span class="yes">Yes</span>
+		    	</label>
+		    	<label>
+ 				 <input type="radio" id="no" name="q4" value="No">
+ 				 <span class="no">No</span>
+		    	</label>
+		    	</div>
+			 
+			 <div class="checkbox4">
+			 	 <p>5.	Tell us honestly if you have coarseness in voice?</p>
+		    	<label>
+			     <input type="radio" id="yes" name="q5" value="Yes" checked>
+				 <span class="yes">Yes</span>
+		    	</label>
+		    	<label>
+ 				 <input type="radio" id="no" name="q5" value="No">
+ 				 <span class="no">No</span>
+		    	</label>
+		    	</div>
+			 
+			 <div class="checkbox5">
+			 	 <p>6.	Tell us honestly if you have sore throat?</p>
+		    	<label>
+			     <input type="radio" id="yes" name="q6" value="Yes" checked>
+				 <span class="yes">Yes</span>
+		    	</label>
+		    	<label>
+ 				 <input type="radio" id="no" name="q6" value="No">
+ 				 <span class="no">No</span>
+		    	</label>
+		    	</div>
+			 
+			 <div class="checkbox6">
+			 	 <p>7.	Tell us honestly if you have shortness of breath?</p>
+		    	<label>
+			     <input type="radio" id="yes" name="q7" value="Yes" checked>
+				 <span class="yes">Yes</span>
+		    	</label>
+		    	<label>
+ 				 <input type="radio" id="no" name="q7" value="No">
+ 				 <span class="no">No</span>
+		    	</label>
+		    	</div>
+			 
+			 <div>
+			 <input type="Submit" class="a1" >
+			 </div>
+			 
+			 </form>
+			 
 
-  2.	Tell us honestly if you have fever?
-  <input type="radio" id="yes" name="q8" value="Yes" checked>
-  <label for="yes">Yes</label><br>
-  <input type="radio" id="no" name="q8" value="No">
-  <label for="no">No</label><br>
+	  </div>
+    
+	  
+    <footer>
+      <div class="footer-container">
+        <div class="left-col">
+          <img src="gg.png" alt="" class="logo">
+          <div class="social-media">
+            <a href="#"><i class="fab fa-facebook-f"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-youtube"></i></a>
+            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+          </div>
+          <p class="rights-text">©dueldutta | All Rights Reserved.</p>
+        </div>
 
-  3.	Tell us honestly if you have headache?
-  <input type="radio" id="yes" name="q9" value="Yes" checked>
-  <label for="yes">Yes</label><br>
-  <input type="radio" id="no" name="q9" value="No">
-  <label for="no">No</label><br>
-
-  4.	Tell us honestly if you have cough and cold?
-  <input type="radio" id="yes" name="q10" value="Yes" checked>
-  <label for="yes">Yes</label><br>
-  <input type="radio" id="no" name="q10" value="No">
-  <label for="no">No</label><br>
-
-  5.	Tell us honestly if you have coarseness in voice?
-  <input type="radio" id="yes" name="q11" value="Yes" checked>
-  <label for="yes">Yes</label><br>
-  <input type="radio" id="no" name="q11" value="No">
-  <label for="no">No</label><br>
-
-  6.	Tell us honestly if you have sore throat?
-  <input type="radio" id="yes" name="q12" value="Yes" checked>
-  <label for="yes">Yes</label><br>
-  <input type="radio" id="no" name="q12" value="No">
-  <label for="no">No</label><br>
-
-  7.	Tell us honestly if you have shortness of breath?
-  <input type="radio" id="yes" name="q13" value="Yes" checked>
-  <label for="yes">Yes</label><br>
-  <input type="radio" id="no" name="q13" value="No">
-  <label for="no">No</label><br>
-  <input type="submit">
-    </form>
-
-</body>
+        <div class="right-col">
+          <h1>COVID19 Updates</h1>
+          <p>&nbsp;</p>
+          <div class="border"></div>
+          <p>&nbsp;</p>
+          <p>Get the letest news of COVID19.</p>
+          <p>&nbsp;</p>
+          <form action="" class="covid19-form">
+            <input type="text" class="txtb" placeholder="Enter Your Email">
+            <input type="submit" class="btn" value="submit">
+          </form>
+        </div>
+      </div>
+    </footer>
+	  
+	  
+  </body>
 </html>
